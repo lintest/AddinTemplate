415af953cff8f2296ec0934f15050c77fad1b2dc
Vitaly Sandalov <s1c@houpack.ru>
2025-06-05 14:10:56 +0300

    У метода AddError модификатор доступа изменен с private на public, чтобы было возможно вызывать исключения в 1с из TestComponent

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 mkdir build32Lin
 cd build32Lin
-cmake -D CMAKE_BUILD_TYPE:STRING=Release -D TARGET_PLATFORM_32:BOOL=ON --build ..
+cmake -D CMAKE_BUILD_TYPE:STRING=Release -D TARGET_PLATFORM_32:BOOL=ON ..
 cmake --build .
 cd ..
 
 mkdir build64Lin
 cd build64Lin
-cmake -D CMAKE_BUILD_TYPE:STRING=Release -D TARGET_PLATFORM_32:BOOL=OFF --build ..
+cmake -D CMAKE_BUILD_TYPE:STRING=Release -D TARGET_PLATFORM_32:BOOL=OFF ..
 cmake --build .
 cd ..
